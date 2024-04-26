567c1ba3a9115321d8df30bd3a8a1c309eeaca18
Kuisong Tong <kuisong.tong@gmail.com>
2024-04-26 07:17:05 -0700

    add gRPC example (#25)

---
+++ examples/grpc/cmd/main.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2024 The nilgo authors
+// Use of this source code is governed by a MIT license found in the LICENSE file.
+
+package main
+
+import (
+	"embed"
+
+	"cloud.google.com/go/compute/metadata"
+
+	"github.com/nil-go/nilgo"
+	"github.com/nil-go/nilgo/config"
+	"github.com/nil-go/nilgo/gcp"
+	ngrpc "github.com/nil-go/nilgo/grpc"
+)
+
+//go:embed config
+var configFS embed.FS
+
+func main() {
+	var opts []any
+	if metadata.OnGCE() {
+		opts = gcp.Options()
+	}
+	opts = append(opts,
+		config.WithFS(configFS),
+		ngrpc.Run(ngrpc.NewServer()),
+	)
+
+	if err := nilgo.Run(opts...); err != nil {
+		panic(err)
+	}
+}
