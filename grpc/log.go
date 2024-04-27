5c4c47f28852c74d1d2ed24d3655aab473e7c7ee
ktong <kuisong.tong@gmail.com>
2024-04-27 09:13:42 -0700

    use init for gRPC log redirection

---
+++ grpc/log.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 The nilgo authors
+// Use of this source code is governed by a MIT license found in the LICENSE file.
+
+package grpc
+
+import (
+	"log/slog"
+
+	"google.golang.org/grpc/grpclog"
+
+	"github.com/nil-go/nilgo/grpc/internal"
+)
+
+func init() { //nolint:gochecknoinits
+	// Redirect gRPC log to slog.
+	grpclog.SetLoggerV2(internal.NewSlogger(slog.Default().Handler()))
+}
