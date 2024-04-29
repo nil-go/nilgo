37211115327b6f9bff16f9d5f9c45fe90160e65a
ktong <kuisong.tong@gmail.com>
2024-04-28 19:05:18 -0700

    add test

---
+++ pprof_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2024 The nilgo authors
+// Use of this source code is governed by a MIT license found in the LICENSE file.
+
+//go:build !race
+
+package nilgo_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/nil-go/nilgo"
+	"github.com/nil-go/nilgo/internal/assert"
+)
+
+func TestPProf(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		assert.NoError(t, nilgo.Run(nilgo.PProf))
+	}()
+	time.Sleep(100 * time.Millisecond) // wait for pprof server to start.
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:6060/debug/pprof/", nil)
+	assert.NoError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	defer func() { _ = resp.Body.Close() }()
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
